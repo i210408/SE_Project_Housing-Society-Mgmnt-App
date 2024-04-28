6697261b0348700e309061be1cbe434bf1cc500a
LeenaRizwan <i210430@nu.edu.pk>
2024-04-28 23:52:59 +0500

    LoginHomeowner
    Implemented, not Designed (yet)

---
+++ SE_Project/UserInterfaceLayer/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogicLayer;
+using Newtonsoft.Json.Linq;
+
+namespace UserInterfaceLayer
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoginH_Click(object sender, EventArgs e)
+        {
+                var controller1 = new Controller();
+            string Name = NameH.Value.ToString();
+                string Pass = PasswH.Value.ToString();
+                if (controller1.Login(Name, Pass) == true)
+                {
+                    Response.Redirect("About.aspx");
+                }
+                else
+                {
+                    Response.Redirect("Default.aspx");
+                }
+            
+        }
+    }
+}
