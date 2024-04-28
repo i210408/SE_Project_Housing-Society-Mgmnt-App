e8a41c2ac0058c1449ad9eeb35d64058693385cb
Enigmatic619 <azhanmuhammad123@gmail.com>
2024-04-28 13:26:47 +0500

    xddd

---
+++ SE_Project/DatabaseLayer/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLayer
+{
+    internal class Class1
+    {
+
+
+        string setname (string name)
+        {
+
+            string b = name;
+            return b;
+
+
+        }
+
+    }
+}
