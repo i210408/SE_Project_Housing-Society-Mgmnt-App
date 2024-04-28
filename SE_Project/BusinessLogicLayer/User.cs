49040495da7adbe352538492d29202cdb6efac54
Hass114 <i210408@nu.edu.pk>
2024-04-28 09:44:00 +0500

    Creating a simple file to test things

---
+++ SE_Project/BusinessLogicLayer/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class User
+    {
+        string userName;
+        string password;
+        User(string userName, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+        }
+    }
+}
