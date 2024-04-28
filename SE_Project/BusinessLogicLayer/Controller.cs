104d2da849a749b2bf9be2ec1756a6caeddde360
Hass114 <i210408@nu.edu.pk>
2024-04-28 16:43:13 +0500

    Added business logic code for login and signup

---
+++ SE_Project/BusinessLogicLayer/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class Controller
+    {
+        // Login to the application.
+        public bool Login(string userName, string password)
+        {
+            if(User.IfUserExists(userName, password))
+                return true;
+            else return false;
+
+        }
+
+        // Create a new account.
+        public void SignUp(string userName, string password, string email, string type)
+        {
+            User.InsertUser(userName, password, email, type);
+        }
+    }
+}
--- SE_Project/BusinessLogicLayer/User.cs
+++ SE_Project/BusinessLogicLayer/User.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DatabaseLayer;
 
 namespace BusinessLogicLayer
 {
-    public class User
+    internal class User
     {
         string userName;
         string password;
-        User(string userName, string password)
+        string email;
+        string type;
+
+        User(string userName, string password, string email, string type)
         {
             this.userName = userName;
             this.password = password;
+            this.email = email;
+            this.type = type;   
         }
 
         public string GetUserName()
@@ -28,6 +34,19 @@ namespace BusinessLogicLayer
         public void SetUsername(string userName)
         {
             this.userName=userName;
+        }
+
+        // Insert new user into the database.
+        public static void InsertUser(string userName, string password, string email, string userType)
+        {
+            Class1.InsertUserData(userName,password,email,userType);
+            Console.WriteLine("Called InsertUserData.");
+        }
+
+        // Check if user exists in database.
+        public static bool IfUserExists(string userName, string password)
+        {
+            return Class1.RetrieveAndCheckUserData(userName,password);
         }
 
     }
