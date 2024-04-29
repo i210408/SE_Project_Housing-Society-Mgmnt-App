a6f4798aab3e55dc71975f7e357f93cba960d887
LeenaRizwan <i210430@nu.edu.pk>
2024-04-29 21:45:08 +0500

    Template Interfaces Made
    Template homepages with space for a sidebar, and Login/Signup pages designed, almost implemented

--- SE_Project/UserInterfaceLayer/LoginH.aspx.cs
+++ SE_Project/UserInterfaceLayer/LoginH.aspx.cs
@@ -9,25 +9,40 @@ using Newtonsoft.Json.Linq;
 
 namespace UserInterfaceLayer
 {
+
     public partial class _Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         protected void LoginH_Click(object sender, EventArgs e)
         {
-                var controller1 = new Controller();
+            var controller1 = new Controller();
             string Name = NameH.Text.ToString();
                 string Pass = PasswH.Text.ToString();
                 if (controller1.Login(Name, Pass) == true)
                 {
-                    Response.Redirect("About.aspx");
+                this.Session["Username"] = NameH.Text.ToString();
+                this.Session["Password"]= PasswH.Text.ToString();
+                this.Session["Controller"] = controller1;
+                if (Homeowner.Checked == true)
+                {
+                    Response.Redirect("HomepageH.aspx");
+                }
+                else if (Admin.Checked == true)
+                {
+                    Response.Redirect("HomepageA.aspx");
                 }
                 else
                 {
-                    Response.Redirect("Default.aspx");
+                    WarningLabel.Text = "Choose Admin or Homeowner";
+                }
+                }
+                else
+                {
+                WarningLabel.Text = "Username/Password Invalid.";
                 }
             
         }
@@ -38,7 +53,11 @@ namespace UserInterfaceLayer
             string Name = NameHS.Text.ToString();
             string Pass = PasswHS.Text.ToString();
             string Email = EmailHS.Text.ToString();
-            controller1.SignUp(Name,Pass, Email, "homeowner");
+            Session[0] = NameHS.Text.ToString();
+            Session[1] = EmailHS.Text.ToString();
+            Session[2] = controller1;
+            //Now go to About as the Homepage
+            //Needs to be changed as per new signup
 
         }
 
