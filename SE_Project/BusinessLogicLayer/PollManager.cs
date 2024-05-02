b603b1e3527564ab7e1658aefdec5ad431d357b5
Hass114 <i210408@nu.edu.pk>
2024-05-02 22:34:24 +0500

    Added Create Polls

---
+++ SE_Project/BusinessLogicLayer/PollManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseLayer;
+
+namespace BusinessLogicLayer
+{
+    internal class PollManager
+    {
+        public static void CreatePoll(string pollQuestion, DateTime startDate, DateTime endDate, string[] options)
+        {
+            DB.CreatePoll(pollQuestion, startDate, endDate, options);
+        }
+    }
+}
